c0ad4c81b7abd01e3851b2d17a624f3ad1534006
Jed <jedwalton98@gmail.com>
2022-08-03 07:46:43 +0100

    2/4

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -6,15 +6,42 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number in decimal system: ");
-        int decimal = scanner.nextInt();
-
-        System.out.print("\nEnter target base: ");
-        int base = scanner.nextInt();
-
-        String result = Integer.toString(decimal, base);
-        System.out.print("\nConversion result:");
-        System.out.print(result);
-
+        boolean cont = true;
+        String choice = "";
+        String number = "0";
+        int base = 0;
+        while (cont) {
+            System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
+            choice = scanner.next();
+            switch (choice) {
+                case "/from" :
+                    System.out.print("Enter a number in decimal system: ");
+                    number = scanner.next();
+                    System.out.print("Enter the target base: ");
+                    base = scanner.nextInt();
+                    if (base == 2 || base == 8 ||base == 16) {
+                        System.out.println("Conversion result: " + Integer.toString(Integer.parseInt(number), base));
+                    } else {
+                        System.out.println("Wrong base");
+                    }
+                    break;
+                case "/to" :
+                    System.out.print("Enter source number:");
+                    number = scanner.next();
+                    System.out.print("Enter the source base: ");
+                    base = scanner.nextInt();
+                    if (base == 2 || base == 8 ||base == 16) {
+                        System.out.println("Conversion to decimal result: "  + Integer.parseInt(number, base));
+                    } else {
+                        System.out.println("Wrong base");
+                    }
+                    break;
+                case "/exit" :
+                    cont = false;
+                    break;
+                default :
+                    System.out.println("Wrong choise");
+            }
+        }
     }
 }
