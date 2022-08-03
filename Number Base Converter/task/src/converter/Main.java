02409540bce8d353376f0d73af8dfb37a88cf31f
Jed <jedwalton98@gmail.com>
2022-08-03 07:48:22 +0100

    stage 3/4

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -1,47 +1,38 @@
 package converter;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean cont = true;
-        String choice = "";
-        String number = "0";
-        int base = 0;
-        while (cont) {
-            System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
-            choice = scanner.next();
-            switch (choice) {
-                case "/from" :
-                    System.out.print("Enter a number in decimal system: ");
-                    number = scanner.next();
-                    System.out.print("Enter the target base: ");
-                    base = scanner.nextInt();
-                    if (base == 2 || base == 8 ||base == 16) {
-                        System.out.println("Conversion result: " + Integer.toString(Integer.parseInt(number), base));
-                    } else {
-                        System.out.println("Wrong base");
-                    }
-                    break;
-                case "/to" :
-                    System.out.print("Enter source number:");
-                    number = scanner.next();
-                    System.out.print("Enter the source base: ");
-                    base = scanner.nextInt();
-                    if (base == 2 || base == 8 ||base == 16) {
-                        System.out.println("Conversion to decimal result: "  + Integer.parseInt(number, base));
-                    } else {
-                        System.out.println("Wrong base");
-                    }
-                    break;
-                case "/exit" :
-                    cont = false;
-                    break;
-                default :
-                    System.out.println("Wrong choise");
+
+        do {
+            System.out.print("Enter two numbers in format: {source base} {target base} (To quit type /exit) ");
+            String maybeSourceBase = scanner.next();
+            if ("/exit".equals(maybeSourceBase)) {
+                return;
             }
-        }
+
+            int targetBase = scanner.nextInt();
+            convertOperation(Integer.parseInt(maybeSourceBase), targetBase, scanner);
+
+        } while (true);
+    }
+
+
+    private static void convertOperation(int sourceBase, int targetBase, Scanner scanner) {
+        do {
+            System.out.printf("Enter number in base %d to convert to base %d (To go back type /back) ", sourceBase, targetBase);
+            String maybeSource = scanner.next();
+            if ("/back".equals(maybeSource)) {
+                return;
+            }
+
+            String target = new BigInteger(maybeSource, sourceBase).toString(targetBase).toUpperCase();
+            System.out.println("Conversion result: " + target);
+
+        } while (true);
     }
 }
